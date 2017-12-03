b396098f56c410e325551105d0a6b3eac9aae6dd
Serhii Onopriienko <serhiionopriienko@Serhiis-MacBook-Pro.local>
2017-12-03 15:42:08 +0200

    Close browser controller by swipe

--- GitHub search/Scenes/Browser/MVCBrowserViewController.swift
+++ GitHub search/Scenes/Browser/MVCBrowserViewController.swift
@@ -34,7 +34,9 @@ class MVCBrowserViewController: UIViewController, MVCBrowserDataPassing, MVCBrow
     var dataStore: MVCBrowserDataStore?
     
     var url: URL?
-
+    
+    private var initialTouchPoint: CGPoint = CGPoint(x: 0,y: 0)
+    
     // MARK: - Lifecycle
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -66,5 +68,31 @@ class MVCBrowserViewController: UIViewController, MVCBrowserDataPassing, MVCBrow
     
     @IBAction func closeAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func panGestureRecognizerHandler(_ sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: self.view?.window)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            initialTouchPoint = touchPoint
+        } else if sender.state == UIGestureRecognizerState.changed {
+            if touchPoint.y - initialTouchPoint.y > 0 {
+                self.view.frame = CGRect(x: 0,
+                                         y: touchPoint.y - initialTouchPoint.y,
+                                         width: self.view.frame.size.width,
+                                         height: self.view.frame.size.height)
+            }
+        } else if sender.state == UIGestureRecognizerState.ended || sender.state == UIGestureRecognizerState.cancelled {
+            if touchPoint.y - initialTouchPoint.y > 100 {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.view.frame = CGRect(x: 0,
+                                             y: 0,
+                                             width: self.view.frame.size.width,
+                                             height: self.view.frame.size.height)
+                })
+            }
+        }
     }
 }
